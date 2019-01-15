ec6fd47b924cc10d6f4658015a59bdaa1b7d587a
sergej <segasle@gmail.com>
2019-01-15 16:26:34 +0300

    добавил модальное окно

--- tempate/header.php
+++ tempate/header.php
@@ -6,8 +6,10 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title></title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css?t=<?php echo(microtime(true).rand()); ?>">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+          integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css?t=<?php echo(microtime(true) . rand()); ?>">
+    <link rel="stylesheet" href="font-icons/css/font-awesome.css">
 </head>
 <body>
 <div class="container-fluid">
@@ -15,15 +17,66 @@
         <div class="header">
             <div class="row">
                 <div class="col-12">
-
                     <div class="logo">
                         <div class="sun move">
                             <div class="roth"></div>
                         </div>
-                        <div class="slang"><p>сверхвидящее</p><p>кострово</p></div>
+                        <div class="slang"><p>сверхвидящее</p>
+                            <p>кострово</p></div>
                     </div>
                     <div class="row">
+                        <div class="col-12">
+                            <button type="button" class="btn btn-primary modal-open" data-toggle="modal"
+                                    data-target="#exampleModal">
+                                <span class="fa fa-user-circle-o fa-1x">Авторизоваться</span>
+                            </button>
+                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
+                                 aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Авторизуйтесь</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form action="" method="post">
+                                                <div class="form-group">
+                                                    <label for="exampleInputEmail1">Email</label>
+                                                    <input type="email" class="form-control" id="exampleInputEmail1" name="email" placeholder="Email">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">Пароль</label>
+                                                    <input type="password" class="form-control" name="password" id="exampleInputPassword1" placeholder="Password">
+                                                </div>
+                                                <div class="checkbox">
+                                                    <label>
+                                                        <input type="checkbox">Запомнить меня
+                                                    </label>
+                                                </div>
+                                                <div class="form-group">
+                                                    <button type="submit" class="btn btn-default btn-primary" name="submit">Войти</button>
+                                                </div>
+                                            </form>
+                                           <?php link_reg(); ?>
+                                            <p class="">Или можно авторизоваться с помощью соц сети</p>
+                                            <div class="block-icons">
+                                                <i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i>
+                                                <i class="fa fa-odnoklassniki fa-2x" aria-hidden="true"></i>
+                                                <i class="fa fa-vk fa-2x" aria-hidden="true"></i>
+                                                <i class="fa fa-instagram fa-2x" aria-hidden="true"></i>
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Отмена</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <div class="col-12 menu-header">
+
                             <div class="menu">
                                 <input type="checkbox" id="checkbox">
                                 <label class="burger" for="checkbox">
@@ -35,6 +88,7 @@
                                     ?>
                                 </nav>
                             </div>
+
                         </div>
                     </div>
                 </div>
