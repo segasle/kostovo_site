9aa1f9c06a537ebc9f42106edc11de3f3fbe9864
sergej <segasle@gmail.com>
2019-01-20 14:23:08 +0300

    Сделал форму для обновление данных пользователя

--- users/profile.php
+++ users/profile.php
@@ -1,7 +1,22 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sergej
- * Date: 17.01.19
- * Time: 16:49
- */
+<h1 class="text-center">Профиль</h1>
+<form action="" method="post" enctype="multipart/form-data">
+    <div class="form-group">
+        .author-phono
+        <label for="exampleInputFile">Загрузите фото</label>
+        <input type="file" id="exampleInputFile" accept="image/jpeg,image/png" name="file">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputName">Имя</label>
+        <input type="text" class="form-control" id="exampleInputName" placeholder="Имя" name="name">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputFamilia">Фамилия</label>
+        <input type="text" class="form-control" id="exampleInputFamilia" placeholder="Фамилия" name="familia">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPhone">Номер телефона</label>
+        <input type="text" class="form-control" id="exampleInputPhone" placeholder="Телефон" name="phone">
+    </div>
+    <button type="submit" class="btn btn-default btn-primary" name="submit">Обновить данные</button>
+
+</form>
