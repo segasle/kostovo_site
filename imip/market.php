1c5350cbff82470f64549a9b19dbe0b93328b13e
sergej <segasle@gmail.com>
2019-01-23 10:18:12 +0300

    Добавил форму для поиска вещей

---
+++ imip/market.php
@@ -0,0 +1,20 @@
+<h1 class="text-center">Барахолка</h1>
+<form method="post" class="row">
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+        <div class="form-group">
+            <select class="form-control" name="value">
+                <option>1</option>
+            </select>
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+        <div class="form-group">
+            <input type="search" class="form-control" name="search" placeholder="Поиск">
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
+        <div class="form-group">
+            <button type="submit" class="btn-primary btn" name="submit">Найти</button>
+        </div>
+    </div>
+</form>
