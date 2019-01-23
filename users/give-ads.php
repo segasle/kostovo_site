d6a8f49c6a277a66115f94f524d4a49adc07e85d
sergej <segasle@gmail.com>
2019-01-23 13:38:35 +0300

    добавил адаптивность

--- users/give-ads.php
+++ users/give-ads.php
@@ -1,24 +1,40 @@
 <h1 class="text-center">Объявление</h1>
-<form method="post" enctype="multipart/form-data">
-    <div class="form-group">
-        <p>Выберите категорию</p>
-        <select class="form-control" name="value">
-            <option>1</option>
-        </select>
+<form method="post" enctype="multipart/form-data" class="row">
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+        <div class="form-group">
+            <p>Выберите категорию</p>
+            <?php
+            selected();
+            ?>
+        </div>
     </div>
-    <div class="form-group">
-        <label for="exampleInputTitle">Заголовок</label>
-        <input type="text" class="form-control" id="exampleInputTitle" placeholder="Заголовок" name="title">
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+        <div class="form-group">
+            <label for="exampleInputTitle">Заголовок</label>
+            <input type="text" class="form-control" id="exampleInputTitle" placeholder="Заголовок" name="title">
+        </div>
     </div>
-    <div class="form-group">
-        <label for="exampleInputPrice">Укажите цену</label>
-        <input type="text" class="form-control" id="exampleInputPrice" placeholder="Цена">
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+        <div class="form-group">
+            <label for="exampleInputPrice">Укажите цену</label>
+            <input type="text" class="form-control" id="exampleInputPrice" placeholder="Цена">
+        </div>
     </div>
-    <div class="form-group">
-        <label for="exampleInputText">Текст</label>
-        <textarea class="form-control" id="exampleInputText" placeholder="Текст"  rows="10"></textarea>
-    </div>                                                                                                                    <div class="form-group">
-        <p>Выберите фото</p>
-        <input type="file" id="exampleInputFile" accept="image/jpeg,image/png" name="file" class="inputfile hide" data-multiple-caption="{count} files selected" multiple> <label for="exampleInputFile" class="btn-primary btn btn-default"><span>Выбрать</span></label></div>
-    <button type="submit" class="btn btn-default btn-primary">Подать</button>
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+        <div class="form-group">
+            <label for="exampleInputText">Текст</label>
+            <textarea class="form-control" id="exampleInputText" placeholder="Текст" rows="10"></textarea>
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+        <div class="form-group">
+            <p>Выберите фото</p>
+            <input type="file" id="exampleInputFile" accept="image/jpeg,image/png" name="file" class="inputfile hide"
+                   data-multiple-caption="{count} files selected" multiple> <label for="exampleInputFile"
+                                                                                   class="btn-primary btn btn-default"><span>Выбрать</span></label>
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+        <button type="submit" class="btn btn-default btn-primary">Подать</button>
+    </div>
 </form>
