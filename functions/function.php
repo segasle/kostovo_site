775279fd11396ad6d9b89427ee01c4d39add5acc
sergej <segasle@gmail.com>
2019-01-24 13:24:39 +0300

    поиск барахолки

--- functions/function.php
+++ functions/function.php
@@ -530,3 +530,50 @@ function add_ads()
     }
     return;
 }
+function search ($query)
+{
+    $query = trim($query);
+    $query = mysqli_real_escape_string($query);
+    $query = htmlspecialchars($query);
+    if (!empty($query))
+    {
+        if (strlen($query) < 3) {
+            $text = '<p>Слишком короткий поисковый запрос.</p>';
+        } else if (strlen($query) > 128) {
+            $text = '<p>Слишком длинный поисковый запрос.</p>';
+        } else {
+            $q = do_query("SELECT `id`, `title`, `text`, `title_link`, `category`, `uniq_id`
+                  FROM `table_name` WHERE `text` LIKE '%$query%'
+                  OR `title` LIKE '%$query%' OR `meta_k` LIKE '%$query%'
+                  OR `meta_d` LIKE '%$query%'");
+
+
+            if (mysqli_affected_rows() > 0) {
+                $row = mysqli_fetch_assoc($q);
+                $num = mysqli_num_rows($q);
+
+                $text = '<p>По запросу <b>'.$query.'</b> найдено совпадений: '.$num.'</p>';
+
+                do {
+                    // Делаем запрос, получающий ссылки на статьи
+                    $q1 = do_query("SELECT `link` FROM `table_name` WHERE `uniq_id` = '$row[page_id]'");
+
+
+                    if (mysqli_affected_rows() > 0) {
+                        $row1 = mysqli_fetch_assoc($q1);
+                    }
+
+                    $text .= '<p><a> href="'.$row1['link'].'/'.$row['category'].'/'.$row['uniq_id'].'" title="'.$row['title_link'].'">'.$row['title'].'</a></p>
+                    <p>'.$row['desc'].'</p>';
+
+                } while ($row = mysqli_fetch_assoc($q));
+            } else {
+                $text = '<p>По вашему запросу ничего не найдено.</p>';
+            }
+        }
+    } else {
+        $text = '<p>Задан пустой поисковый запрос.</p>';
+    }
+
+    return $text;
+}
