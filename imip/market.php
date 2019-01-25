1414aaa08cf2023ffb4f57c49fc932161d7d4adc
sergej <segasle@gmail.com>
2019-01-25 14:14:06 +0300

    вывел функцию поиска в нуужном месте

--- imip/market.php
+++ imip/market.php
@@ -1,6 +1,5 @@
 <h1 class="text-center">Барахолка</h1>
 <?php
-search();
 ?>
 <form method="post" class="row">
     <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
@@ -23,31 +22,39 @@ search();
 </form>
 <div class="row">
     <?php
-    $post = do_query("SELECT * FROM `ads`");
-    while ($us = mysqli_fetch_assoc($post)) {
-        $title = $us['title'];
-        $text = $us['text'];
-        $price = $us['price'];
-        $data = new DateTime($us['date']);
-        if (!empty($us['photo'])) {
-            $img = '<img src="ads_img/' . $us['photo'] . '" class="post_img">';
-        } else {
-            $img = '<div class="post_no-img"><p>Нет фото</p></div>';
-        }
-        ?>
+    if (isset($_POST['submit'])){
+        search();
+    }else{
+        $post = do_query("SELECT * FROM `ads`");
+        while ($us = mysqli_fetch_assoc($post)) {
+            $title = $us['title'];
+            $text = $us['text'];
+            $price = $us['price'];
+            $data = new DateTime($us['date']);
+            if (!empty($us['photo'])) {
+                $img = '<img src="ads_img/' . $us['photo'] . '" class="post_img">';
+            } else {
+                $img = '<div class="post_no-img"><p>Нет фото</p></div>';
+            }
+            ?>
 
-        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2">
-            <div class="post"><?php echo $img; ?>
-                <div class="post_title">
-                    <p><?php echo $title;?></p>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2">
+                <div class="post"><?php echo $img; ?>
+                    <div class="post_title">
+                        <p><?php echo $title;?></p>
+                    </div>
+                    <div class="post_text"><p><?php echo $text;?></p></div>
+                    <div class="post_price"><p class="fa fa-rub"><?php echo $price;?></p></div>
+                    <div class="post_data"><p><?php echo $data->format('d:m:Y H:m:s');?></p></div>
                 </div>
-                <div class="post_text"><p><?php echo $text;?></p></div>
-                <div class="post_price"><p class="fa fa-rub"><?php echo $price;?></p></div>
-                <div class="post_data"><p><?php echo $data->format('d:m:Y H:m:s');?></p></div>
             </div>
-        </div>
 
-        <?php
-    } ?>
+            <?php
+        }
+
+
+    }
+
+    ?>
 </div>
 
