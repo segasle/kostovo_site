230cfa7a053b1ec39861a8dd0f94d87573b1f8c2
sergej <segasle@gmail.com>
2019-01-30 19:46:03 +0300

    Добавил файл для api instagram

---
+++ api/instagram.php
@@ -0,0 +1,2 @@
+<?php
+$clent_id = '609aa4812e9b41cb8036087b1b6fa8ed';
--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 require 'functions/bd.php';
 require 'functions/function.php';
 require 'api/vk.php';
+require 'api/instagram.php';
 vk_authorization();
 connections();
