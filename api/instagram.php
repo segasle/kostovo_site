498b42fd726814d9e3a5ac15360bf4e338ce3847
sergej <segasle@gmail.com>
2019-01-30 20:20:37 +0300

    токен для instagram

--- api/instagram.php
+++ api/instagram.php
@@ -1,2 +1,6 @@
 <?php
 $clent_id = '609aa4812e9b41cb8036087b1b6fa8ed';
+$token_instagram = '#access_token=1396870575.609aa48.5d7512c8eeb44e029bb5cd4b97b1cdf0';
+global $redirect_uri;
+$link = '<a href="https://www.instagram.com/oauth/authorize/?client_id='.$clent_id.'&redirect_uri='.$redirect_uri.'&response_type=token"></a>';
+echo $link;
