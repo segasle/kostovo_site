666e50720f274e72e4819c06505537211f34ca5a
sergej <segasle@gmail.com>
2019-01-30 17:18:34 +0300

    Добавил форму для добавления постов

--- imip/history.php
+++ imip/history.php
@@ -1,3 +1,26 @@
 <h1 class="text-center">История</h1>
 <?php
+if (isset($_SESSION['token'])) {
+    ?>
+    <h2 class="h4">Вы можете написать пост</h2>
+    <form class="form-horizontal" method="post">
+        <div class="form-group">
+            <label for="exampleInputText">Текст</label>
+            <textarea class="form-control" id="exampleInputText" placeholder="Текст" rows="10" name="text"></textarea>
+        </div>
+        <div class="form-group">
+            <p>Выберите фото</p>
+            <input type="file" id="exampleInputFile" accept="image/jpeg,image/png" name="file" class="inputfile hide"
+                   data-multiple-caption="{count} files selected" multiple> <label for="exampleInputFile"
+                                                                                   class="btn-primary btn btn-default"><span>Выбрать</span></label>
+        </div>
+        <button type="submit" class="btn btn-default btn-primary">Отправить</button>
+
+    </form>
+    <?php
+} else {
+    ?>
+    <h2 class="text-center">Авторизуйтесь через вк пожалуйста, чтобы написать пост</h2>
+    <?php
+}
 get_post_vk();
