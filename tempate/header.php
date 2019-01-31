ce3c66d7d45284537189a837644a2e2157666c62
sergej <segasle@gmail.com>
2019-01-31 10:09:30 +0300

    добавил js скрипт для api facebook

--- tempate/header.php
+++ tempate/header.php
@@ -10,6 +10,27 @@
           integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css?t=<?php echo(microtime(true) . rand()); ?>">
     <link rel="stylesheet" href="font-icons/css/font-awesome.css">
+    <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '1759331964212959',
+                cookie     : true,
+                xfbml      : true,
+                version    : 'v3.2'
+            });
+
+            FB.AppEvents.logPageView();
+
+        };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
 </head>
 <body>
 <div class="container-fluid">
