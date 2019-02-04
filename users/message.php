5ac9560a420bbc543b64f71a986b40d098a447e7
sergej <segasle@gmail.com>
2019-02-04 17:33:47 +0300

    шаблон сообщение

--- users/message.php
+++ users/message.php
@@ -1,3 +1,19 @@
+<h1 class="text-center">Сообщения</h1>
+<div class="row">
+    <div class="col-3 col-xs-12"></div>
+    <div class="col-9 col-xs-12">
+        <div class="message">
+            <div class="message-window">
+                <p class="window-text"></p>
+            </div>
+        </div>
+        <form action="" method="post" class="form-message">
+            <div class="form-group">
+                <input type="text" class="form-control"><button type="submit" class="fa fa-paper-plane-o fa-2x"></button>
+            </div>
+        </form>
+    </div>
+</div>
 <?php
 /**
  * Created by PhpStorm.
