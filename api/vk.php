482ecc4e556c1ff456c23af1e9234dca00c2e3c6
sergej <segasle@gmail.com>
2019-02-13 11:08:02 +0300

    Добавил wall и сменил токен

--- api/vk.php
+++ api/vk.php
@@ -1,10 +1,10 @@
 <?php
 $appkey = 'qG0KCSlZ7x5VblkcR3z6';
 $redirect_uri = 'http://localhost:8888/kostovo_site/';
-$token = 'access_token=6b3dcb09a02d5b169df16faeb42f9c192a94b804697aaf1b1cc17bc9289c46893523fe04436fa7731d46b';
+$token = 'access_token=e68531cae52f33044a9529a3c05096c30ddb38fdef1ff5aa5b5bc7dc2c8d6ff9290996e46ddeef10f6f00';
 $token2 = 'access_token=48188f4b3d31e87bee34497e19813a6245ec18cee7522d098b1c2d2be2939ae418ba9ab7333c429e07a26';
 $id ='6813787';
-$scope = 'friends,photos,audio,video,offline,stats,market,docs,notes,stories,status,groups,email';
+$scope = 'friends,photos,audio,video,offline,stats,market,docs,notes,stories,status,groups,email,wall';
 $users = 'photo_max,first_name,last_name,city';
 $owner_id = '-70567817';
 $album_id = '194340901';
