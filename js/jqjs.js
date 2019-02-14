01378ad738c51400e7fa77ac3e6282b8a8daf9b9
sergej <segasle@gmail.com>
2019-02-14 20:01:14 +0300

    логика модального окна

--- js/jqjs.js
+++ js/jqjs.js
@@ -1,22 +1,26 @@
 
-(function($) {
-    $(function() {
-        if (!$.cookie('smartCookies')) {
+jQuery(document).ready(function ($) {
+    /* show */
+    $('#appomed-overlay').css('visibility', 'visible');
+    $('#appomed-overlay').css('opacity', '1');
+    $('#appomed-pop-main').css('visibility', 'visible');
+    $('#appomed-pop-main').css('opacity', '1');
 
-            function getWindow(){
-                $('.offer').arcticmodal({
-                    closeOnOverlayClick: false,
-                    closeOnEsc: true
-                });
-            };
+    /* click to close */
+    $('#appomed-pop-close').click(appomedclosepop);
+    $('#appomed-pop-but').click(appomedclosepop);
 
-            setTimeout (getWindow, 5000);
-        }
+    /* close function */
+    function appomedclosepop() {
+        setTimeout($('#appomed-overlay').css('visibility', 'hidden'), 500);
+        $('#appomed-overlay').css('opacity', '0');
+        setTimeout($('#appomed-pop-main').css('visibility', 'hidden'), 500);
+        $('#appomed-pop-main').css('opacity', '0');
+        var cookieName = 'ap';
+        var cookieValue = 'ap';
+        var myDate = new Date();
+        myDate.setMonth(myDate.getMonth() + 12);
+        document.cookie = cookieName + "=" + cookieValue + "; path=/; expires=" + myDate;
 
-        $.cookie('smartCookies', true, {
-            expires: 180,
-            path: '/'
-        });
-
-    })
-})(jQuery)
+    }
+});
