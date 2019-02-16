cac66283b61c3a815b4b2163f37574804546b9cf
segasle <segasle@gmail.com>
2019-02-16 17:31:11 +0300

    парсер мероприятий упрасил

--- imip/event.php
+++ imip/event.php
@@ -1,7 +1,9 @@
 <h1 class="text-center">Мероприятия</h1>
 <?php
+$link = 'https://www.kostrovodk.ru/meropriyatiya';
+echo '<p class="h5">Контент взят с официального сайта <a href="'.$link.'">ДК  Кострово</a><p>';
 require 'panser/simple_html_dom.php';
-$content = file_get_html("https://www.kostrovodk.ru/meropriyatiya");
+$content = file_get_html("$link");
 
 foreach ($content->find('div#comp-in8s4jllinlineContent') as $item){
     echo $item;
