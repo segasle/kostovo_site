34d66ad706e90f3327fb6eddbb3d5f09cf5079c7
segasle <segasle@gmail.com>
2019-02-17 13:38:07 +0300

    Проверки на заполнения формы

--- imip/contant.php
+++ imip/contant.php
@@ -5,28 +5,57 @@
     <p class="fa fa-mail-forward"><a href="mailto:<?php echo $email;?>"><?php echo $email;?></a></p>
 </div>
 <h3 class="text-center">Обратная связь</h3>
-<form class="form-horizontal">
+<form method="post" class="form-horizontal">
     <div class="form-group">
         <label for="inputEmail3" class="col-sm-2 control-label">Ваше имя</label>
         <div class="col-sm-10">
-            <input type="email" class="form-control" id="inputEmail3" name="name" placeholder="Имя">
+            <input type="text" class="form-control" id="inputEmail3" name="name" placeholder="Имя" value="<?php echo @$_POST['name'];?>">
         </div>
     </div>
     <div class="form-group">
         <label for="inputPassword3" class="col-sm-2 control-label">Email</label>
         <div class="col-sm-10">
-            <input type="password" class="form-control" id="inputPassword3" name="email" placeholder="Email">
+            <input type="email" class="form-control" id="inputPassword3" name="email" placeholder="Email" value="<?php echo @$_POST['email'];?>">
         </div>
     </div>
     <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10">
             <p>Сообщение</p>
-            <textarea class="form-control" rows="3" name="text" placeholder="Текст"></textarea>
+            <textarea class="form-control" rows="3" name="text" placeholder="Текст"><?php echo @$_POST['text'];?></textarea>
         </div>
     </div>
     <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit" class="btn btn-default btn-primary">Отправить</button>
+            <button type="submit" name="submit" class="btn btn-default btn-primary">Отправить</button>
         </div>
     </div>
 </form>
+<?php
+if (isset($_POST['submit'])){
+    $data = $_POST;
+    $errors = array();
+    $emailUser = $data['email'];
+    if (empty($data['name']) or $data['name'] == ' '){
+        $errors[] = 'Не ввели имя';
+    }
+    if ($data['name'] <= 3 or $data['name'] >= 20){
+        $errors[] = 'Имя должно быть не меньше 3 симловов и не больше 20';
+    }
+    if (empty($data['email'])){
+        $errors[] = 'Не ввели почту';
+    }
+    if (!preg_match("/^(?!.*@.*@.*$)(?!.*@.*\-\-.*\..*$)(?!.*@.*\-\..*$)(?!.*@.*\-$)(.*@.+(\..{1,11})?)$/", "$emailUser")) {
+        $errors[] = 'Вы неправильно ввели электронную почту';
+    }
+    if (empty($data['text']) or $data['text'] == ' '){
+        $errors[] = 'Поле сообщение пустое';
+    }
+    if ($data['text'] <= 10 or $data['text'] >= 1000) {
+        $errors[] = 'Имя должно быть не меньше 3 симловов и не больше 20';
+    }
+    if (empty($errors)){
+
+    }else{
+        echo '<div class="errors">'.array_shift($errors).'</div>';
+    }
+}
