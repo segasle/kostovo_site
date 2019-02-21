6cb208820650c1adc3ad73a45507045f1634730f
segasle <segasle@gmail.com>
2019-02-21 14:18:36 +0300

    переестил в файл функцию

--- imip/history.php
+++ imip/history.php
@@ -8,13 +8,14 @@ if (isset($_SESSION['token'])) {
             <label for="exampleInputText">Текст</label>
             <textarea class="form-control" id="exampleInputText" placeholder="Текст" rows="10" name="text"></textarea>
         </div>
-      <!--  <div class="form-group">
-            <p>Выберите фото</p>
-            <input type="file" id="exampleInputFile" accept="image/jpeg,image/png" name="file" class="inputfile hide"
-                   data-multiple-caption="{count} files selected" multiple> <label for="exampleInputFile"
-                                                                                   class="btn-primary btn btn-default"><span>Выбрать</span></label>
-        </div>-->
-        <div class="form-group"><label for="chexkbox"><input id="chexkbox" type="checkbox" name="chexkbox" value="1">Анон</label></div>
+        <!--  <div class="form-group">
+              <p>Выберите фото</p>
+              <input type="file" id="exampleInputFile" accept="image/jpeg,image/png" name="file" class="inputfile hide"
+                     data-multiple-caption="{count} files selected" multiple> <label for="exampleInputFile"
+                                                                                     class="btn-primary btn btn-default"><span>Выбрать</span></label>
+          </div>-->
+        <div class="form-group"><label for="chexkbox"><input id="chexkbox" type="checkbox" name="chexkbox" value="1">Анон</label>
+        </div>
         <button type="submit" class="btn btn-default btn-primary" name="submit">Отправить</button>
 
     </form>
@@ -25,24 +26,24 @@ if (isset($_SESSION['token'])) {
     <?php
 }
 
-if (isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $data = $_POST;
     $errors = array();
-    if (empty($data['text'])){
+    if (empty($data['text'])) {
         $errors[] = 'Вы не ввели сообщения';
     }
-    if (empty($errors)){
+    if (empty($errors)) {
         global $owner_id;
-        if (isset($data['checkbox']) && $data['chexkbox'] == 1){
-            $content = file_get_contents("https://api.vk.com/method/wall.post?owner_id=$owner_id&message='".$data['text']."'&signed=0&'".$_SESSION['token']."'&v=5.70");
+        if (isset($data['checkbox']) && $data['chexkbox'] == 1) {
+            $content = file_get_contents("https://api.vk.com/method/wall.post?owner_id=$owner_id&message='" . $data['text'] . "'&signed=0&'" . $_SESSION['token'] . "'&v=5.70");
             $js = json_decode($content, true);
-            if ($js){
+            if ($js) {
                 echo '<div class="go">Успешно отправленол</div>';
             }
-        }else{
-            $content = file_get_contents("https://api.vk.com/method/wall.post?owner_id=$owner_id&message='".$data['text']."'&signed=1&'".$_SESSION['token']."'&v=5.70");
+        } else {
+            $content = file_get_contents("https://api.vk.com/method/wall.post?owner_id=$owner_id&message='" . $data['text'] . "'&signed=1&'" . $_SESSION['token'] . "'&v=5.70");
             $js = json_decode($content, true);
-            if ($js){
+            if ($js) {
                 echo '<div class="go">Успешно отправленол</div>';
             }
         }
@@ -53,8 +54,52 @@ if (isset($_POST['submit'])){
 
             echo '<div class="go">Успешно отправлено</div>';
         } */
-    }else{
-        echo '<div class="errors">'.array_shift($errors).'</div>';
+    } else {
+        echo '<div class="errors">' . array_shift($errors) . '</div>';
     }
 }
-get_post_vk();
+
+global $token;
+global $owner_id;
+$out = '<div class="container"><div class="row">';
+$content2 = file_get_contents("https://api.vk.com/method/wall.get?owner_id=$owner_id&count=100&extended=1&filter=all&$token&v=5.60");
+$elements2 = json_decode($content2, true);
+foreach ($elements2 as $value) {
+    foreach ($value as $profile) {
+        $fio = $profile['profiles']['first_name'] . ' ' . $profile['profiles']['last_name'];
+        if (isset($profile['profiles']['screen_name'])) {
+            $link = 'https://vk.com/' . $profile['screen_name'];
+        }
+        //return $fio;
+        //global $fio;
+        //exit($fio);
+        //break;
+    }
+
+
+    foreach ($value['items'] as $item) {
+        $link_post = $item['from_id'] . '_' . $item['id'];
+        $data = date('d.m.Y h:m', $item['date']);
+        $text = $item['text'];
+        if (isset($item['attachments'])) {
+            if (is_array($item['attachments']) || is_object($item['attachments'])) {
+                foreach ($item['attachments'] as $key => $values) {
+                    if (isset($values['photo'])) {
+                        $img = $values['photo']['photo_604'];//;
+                    } else {
+                        $img = '';
+                    }
+                    //unset($img);
+                    //break;
+                }
+                $out .= '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><div class="post"><p class="post_text">' . $text . '</p>' . '<img src="' . $img . '" class="post_img">' . '<a class="post_link" href="' . @$link . '" target="_blank">' . $fio . '</a>' . '<p class="post_data">' . $data . '</p><a href="https://vk.com/wall' . $link_post . '" target="_blank">Ссылка на пост</a></div></div>';
+
+            }
+
+        }
+    }
+}
+$out .= '</div></div>';
+echo $out;
+
+//echo get_post_vk();
