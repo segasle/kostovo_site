643d38693e21fd306e1bec458e0e7b540767f16f
segasle <segasle@gmail.com>
2019-02-25 12:12:55 +0300

    добавил функцию мета title заголовки

--- functions/head.php
+++ functions/head.php
@@ -1,7 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sergej
- * Date: 08.01.19
- * Time: 10:55
- */
+$meta = do_query("SELECT * FROM `meta_title`");
+foreach ($meta as $item){
+
+    global $title;
+    if (basename($_SERVER['REQUEST_URI']) == '?page=main' or basename($_SERVER['REQUEST_URI']) == $item['page']) {
+        $title = $item['title'];
+    }
+}
