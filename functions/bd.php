c0860b740caa1109d8913471d5e81725678b4950
segasle <segasle@gmail.com>
2019-02-27 07:19:20 +0300

    ОПптировал бд

--- functions/bd.php
+++ functions/bd.php
@@ -1,13 +1,25 @@
 <?php
+$array = array(
+    array(
+        'host' => 'localhost',
+        'user' => 'root',
+        'password' => 'root',
+        'database' => 'kostovo',
+    ),
+    array(
+        'host' => 'localhost',
+        'user' => 'ca57629_kostrovo',
+        'password' => 'Nexvf1998',
+        'database' => 'ca57629_kostrovo',
+    ),
+);
 global $mysqli;
-if (empty($mysqli)){
-    $mysqli = mysqli_connect('localhost', 'ca57629_kostrovo', 'Nexvf1998', 'ca57629_kostrovo');
-    mysqli_set_charset($mysqli, 'UTF8');
-}if (empty($mysqli)){
-    $mysqli = mysqli_connect('localhost', 'root', 'root', 'kostovo');
-    mysqli_set_charset($mysqli, 'UTF8');
+foreach ($array as $item){
+    if (empty($mysqli)){
+        $mysqli = mysqli_connect($item['host'], $item['user'], $item['password'], $item['database']);
+        mysqli_set_charset($mysqli, 'UTF8');
+    }
+    if (mysqli_connect_errno()){
+        echo 'ошибка в подключении к БД ('.mysqli_connect_errno().')'.mysqli_connect_error();
+    }session_start();
 }
-
-if (mysqli_connect_errno()){
-    echo 'ошибка в подключении к БД ('.mysqli_connect_errno().')'.mysqli_connect_error();
-}session_start();
