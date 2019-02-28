df651931ce7c662be40bc6a8c5588d31004f56fe
sergej <segasle@gmail.com>
2019-02-28 17:54:40 +0300

    подготовка html

--- users/settings.php
+++ users/settings.php
@@ -1,7 +1,15 @@
 <h1 class="text-center">Настройки</h1>
-<p class="h5">Сменить пароль</p>
+
 <form action="" method="post">
-    <div class="group"></div>
+    <div class="form-group">
+        <p class="h5">Сменить пароль</p>
+        <label for="passwordR">Старый пароль</label>
+        <div class="form-group_input">
+            <input type="password" class="form-control" id="passwordR">
+            <div class="input-group-addon" id="s-h-pass"><span class="fa fa-eye fa-2x" title="Показать пароль"></span></div>
+        </div>
+
+    </div>
 </form>
 <?php
 /**
